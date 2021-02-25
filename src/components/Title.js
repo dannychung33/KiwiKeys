a6ae913b0f999734095ba5355977778159b9fd47
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2021-02-24 23:42:03 -0600

    did my thang
    so good

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import '../App.css'
+import Power from './Power'
+
+const Title = () => {
+    return(
+    <h3 className="title"
+        style={{color:"yellowgreen"}}>
+        Kiwi 
+        <Power />
+        Keys
+    </h3>
+    )
+}
+
+export default Title
